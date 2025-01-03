72ccc2aa8764333fd8f8877c6d1b7d65d37e026f
FerdinandBujanowski <ferdinand-bujanowski@web.de>
2025-01-03 16:23:59 +0100

    Renamed DummyStrategy to MyStrategy

--- code/strategies/dummy_strategy.py
+++
@@ -1,9 +0,0 @@
-from strategies.master_strategy import GameStrategy
-
-class DummyStrategy(GameStrategy):
-
-    def __init__(self):
-        super().__init__()
-
-    def next_move(self):
-        print("Dummy Strategy next move.")
---
+++ code/strategies/my_strategy.py
@@ -0,0 +1,9 @@
+from strategies.master_strategy import GameStrategy
+
+# you can rename the class, as long as it extends GameStrategy
+class MyStrategy(GameStrategy):
+
+    def next_move(self):
+
+        # TODO code your own strategy
+        pass
